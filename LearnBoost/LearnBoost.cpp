5e7670fb0bc528ced919cab406ad521519a5e65d
Cihan Ulas <culas@isravision.com>
2020-12-01 13:01:42 +0300

    add boost optional usage.

--- LearnBoost/LearnBoost.cpp
+++ LearnBoost/LearnBoost.cpp
@@ -2,6 +2,7 @@
 #include <boost/any.hpp>
 #include <boost/lambda/lambda.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/optional.hpp>
 #include <boost/regex.hpp>
 #include <boost/variant.hpp>
 #include <iostream>
@@ -140,7 +141,76 @@ void example_any() {
   std::vector<Property> properties;
 }
 
+void example_optional() {
+  std::vector<char> queue;
+
+  // return null is not convinient.
+  auto get_async_data = [&queue]() {
+    if (!queue.empty())
+      return queue.back();
+    else
+      return '\0';  // this is valid char.
+  };
+
+  // so we can use boost::variant<nullptr_t, char> v;
+
+  // optional is more convinent than this.
+
+  boost::optional<char> op;  // op is uninitialized, no char is constructed.
+  op = 'A';
+
+  auto get_async_data_with_optional = [&queue]() {
+    if (!queue.empty())
+      return boost::optional<char>(queue.back());
+    else
+      return boost::optional<char>();
+  };
+
+  op = get_async_data_with_optional();
+  if (!op)
+    std::cout << "queue is empty";
+  else {
+    std::cout << " queue has value of" << op.get()
+              << std::endl;  // THIS WILL CRASH IF optional is uninitialized.
+    std::cout << " queue has value of" << *op
+              << std::endl;  // THIS WILL CRASH IF optional is uninitialized.
+  }
+
+  op.reset();  // reset op to uninitialized state.
+
+  // if we want get a default value in case of uninitialized state.
+  std::cout << op.get_value_or('z') << std::endl;
+
+  char* p = op.get_ptr();  // return null if op is empty.
+
+  // optional can store any kind of data.
+  struct A {
+    std::string name;
+    int b;
+  };
+  A a;
+  boost::optional<A> opA0;    // constructor of A is not called.
+  boost::optional<A> opA(a);  // a is copy constructor into opA.
+  std::cout << opA->name << std::endl;
+
+  // Pointer
+  boost::optional<A*> opAP(&a);
+  (*opAP)->name = "Cex";
+
+  // Referece
+  boost::optional<A&> opAR(a);
+  opAR->name = "Ali";
+
+  // Releational Operator
+  boost::optional<int> i1(1);
+  boost::optional<int> i2(9);
+  boost::optional<int> i3;  // uninitialized considered as smallest.
+
+  if (i1 > i2) std::cout << "greater" << std::endl;
+}
+
 int main() {
+  example_optional();
   example_any();
   example_variant();
   example_lexical_cast();
